a6d40333d5aa409436ccddd6660f0a09f6ebd5b6
avelareliandro <eliandro_avelar@yahoo.com.br>
2026-02-15 20:58:47 -0300

    ultima atualização

--- aula4.py
+++ aula4.py
@@ -6,7 +6,7 @@ class Paciente:
         self.peso = peso
         self.tipo_sanguineo = tipo_sanguineo
         self.queixa = queixa
-        self.dat_consult = dat_consult if isinstance(dat_consult, list) else ([dat_consult] if dat_consult is not None else [])
+        self.dat_consult = dat_consult if isinstance(dat_consult, list) else ([dat_consult] if dat_consult is not None else []) # O que essa nova linha faz (Passo a passo):Primeiro teste: É uma lista? Se sim, usa ela mesma. Segundo teste (se não for lista): O valor é diferente de None? Se for algo como uma única data "10/02", ele coloca nos colchetes: ["10/02"]. Se for None (vazio), ele entrega uma lista vazia: [].
 
     def envelhecer(self):
         self.idade += 1
