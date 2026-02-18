a342d51c6c6bccefcbf029a35a31bfc17eef5ca5
avelareliandro <eliandro_avelar@yahoo.com.br>
2026-02-18 20:09:44 -0300

    vova alteração

--- aula4.py
+++ aula4.py
@@ -8,14 +8,17 @@ class Paciente:
         self.queixa = queixa
         self.dat_consult = dat_consult if isinstance(dat_consult, list) else ([dat_consult] if dat_consult is not None else []) # O que essa nova linha faz (Passo a passo):Primeiro teste: É uma lista? Se sim, usa ela mesma. Segundo teste (se não for lista): O valor é diferente de None? Se for algo como uma única data "10/02", ele coloca nos colchetes: ["10/02"]. Se for None (vazio), ele entrega uma lista vazia: [].
 
-    def envelhecer(self):
-        self.idade += 1
-
-    def engordar(self, kg):
-        self.peso += kg
-
-    def emagrecer(self, kg):
-        self.peso -= kg
-
-    def __str__(self):
-        return f"Paciente: {self.nome}, Idade: {self.idade}, Peso: {self.peso}kg"
+    def altera_idade(self, nova_idade):
+        self.idade = nova_idade
+    
+    def altera_altura(self, nova_altura):
+        self.altura = nova_altura   
+    def altera_peso(self, novo_peso):
+        self.peso = novo_peso
+    def altera_tipo_sanguineo(self, novo_tipo_sanguineo):
+        self.tipo_sanguineo = novo_tipo_sanguineo
+    def altera_queixa(self, nova_queixa):
+        self.queixa = nova_queixa
+    def altera_dat_consult(self, nova_dat_consult):
+        self.dat_consult.append(nova_dat_consult)
+    
